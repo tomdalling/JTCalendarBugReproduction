a0f297fcf5197d647547029a73e8378e4739b75d
Tom Dalling <tom@tomdalling.com>
2015-10-28 17:09:55 +1100

    Initial Commit

---
+++ JTCalendarBugReproduction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTCalendarBugReproduction
+//
+//  Created by Tom on 28/10/2015.
+//  Copyright © 2015 Tom Dalling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTCalendarBugReproduction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JTCalendarBugReproduction
+//
+//  Created by Tom on 28/10/2015.
+//  Copyright © 2015 Tom Dalling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
